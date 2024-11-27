360840571d5aaf79109fa0ba44bd6c74c44a8fe5
DevPrahlad <prahlad.singh.education@gmail.com>
2024-11-27 22:07:22 +0530

    deploy

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,11 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors());
+const corsOptions = {
+    origin: 'http://localhost:5173',
+    credentials: true
+}
+app.use(cors(corsOptions));
 
 const __dirname = path.resolve();
 console.log(__dirname);
@@ -28,10 +32,11 @@ app.use('/api/auth', authRoutes);
 app.use('/api/blogs', blogRoutes);
 app.use('/api/subscriptions', subscriptionRoutes);
 
-// Root Route
-app.get('/', (req, res) => {
-    res.send('API is running...');
-});
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.get("*", (req,res)=>{
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+})
+
 
 // Start Server
 const PORT = process.env.PORT || 1000;
